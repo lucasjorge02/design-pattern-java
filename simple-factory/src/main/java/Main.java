fc9ba7a476b4f7f7d837cb3a00ef5f024bc1320b
Lucas Barros Jorge <lucas.jorge@sptech.school>
2025-12-07 14:49:51 -0300

    featÃ: colocando mais uma instacia de desodorante

--- simple-factory/src/main/java/Main.java
+++ simple-factory/src/main/java/Main.java
@@ -4,7 +4,11 @@ import model.Desodorante;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Aplicando um Desodorante Aerossol");
-        Desodorante desodorante = DesodoranteSimpleFactory.aplicarDesodorante("Aerossol");
-        System.out.println(desodorante);
+        Desodorante desodorante1 = DesodoranteSimpleFactory.aplicarDesodorante("Aerossol");
+        System.out.println(desodorante1);
+
+        System.out.println("Aplicando um Desodorante Creme");
+        Desodorante desodorante2 = DesodoranteSimpleFactory.aplicarDesodorante("Creme");
+        System.out.println(desodorante2);
     }
 }
