193da9fbdd3248abafca5e5cbd84cbba51dca9c1
Lucas Barros Jorge <lucas.jorge@sptech.school>
2025-12-07 21:09:02 -0300

    fix: reestruturando pastas pelo CLI

--- simple-factory/src/main/java/model/DesodoranteRollOn.java
+++
@@ -1,9 +0,0 @@
-package model;
-
-public class DesodoranteRollOn extends Desodorante{
-    @Override
-    public void aplicar() {
-        System.out.println("Removendo tampa do Roll-on");
-        System.out.println("Deslizando desodorante na região das axilas!");
-    }
-}
